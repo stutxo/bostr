406641d730eadaefaa65079751926aa0e7a67b9d
stutxo <70952638+stutxo@users.noreply.github.com>
2025-12-07 17:17:43 -0500

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# bostr
+# bitcoin and other stuff transmitted by relays (BOSTR) (PoC)
+
+## Plebfi dec 7th 2025 hackathon submission
+
+We use ZMQ and nostr to relay bitcoin transactions between bitcoin nodes
+
+uses ben carmans "nip-85" https://github.com/nostr-protocol/nips/pull/476
+
+Bostr will listen to the `rawtx` bitcoin core ZMQ topic and forward transactions over nostr using kind(28333)
+
+If there is another Bostr node running somewhere, it will recieve the ephemeral note containing the batched bitcoin transacations and submit the raw transactions to its node
+
+# How i setup the demo
+
+```
+./build/bin/bitcoind -datadir=<datadir1> -zmqpubrawtx=tcp://127.0.0.1:28332
+```
+
+blocks only mode and clear the mempool to easily show the demo
+```
+./build/bin/bitcoind -datadir=<datadir2> -port=38333 -rpcport=38332 -rpcbind=0.0.0.0 -zmqpubrawtx=tcp://127.0.0.1:28342 -persistmempool=0 -blocksonly=1
+````
+
+```
+cargo run -- --bitcoinrpc http://127.0.0.1:8332 --datadir <datadir1> --zmq tcp://127.0.0.1:28332
+```
+
+```
+cargo run -- --bitcoinrpc http://127.0.0.1:38332 --datadir <datadir2> --zmq tcp://127.0.0.1:38332
+```
+
+Inspired by
+
+https://github.com/benthecarman/nostr-tx-broadcast
+
+https://gnusha.org/pi/bitcoindev/CAJBJmV932eeuiBzo_EMxJ1iU=Gave9=PC3U7seVoBXUFsu_GUA@mail.gmail.com/
+
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,122 +1,165 @@
-use bitcoin::consensus::Decodable;
-
-use std::thread;
-
-use bitcoin::Transaction;
+use anyhow::{Context, anyhow};
+use base64::prelude::*;
+use bitcoin::{Transaction, consensus::Decodable};
+use bitcoind_async_client::Client as BitcoinClient;
+use bitcoind_async_client::traits::Broadcaster;
+use clap::Parser;
 use nostr_sdk::prelude::*;
+use std::fs;
+use std::thread;
 use tokio::sync::mpsc;
 use tracing::{error, info};
+
+#[derive(Parser, Debug)]
+struct Cli {
+    #[arg(long)]
+    zmq: String,
+    #[arg(long)]
+    bitcoinrpc: String,
+    #[arg(long)]
+    datadir: String,
+}
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt().without_time().init();
+    let args = Cli::parse();
 
-    let (tx, rx) = mpsc::channel(100);
-    let nostr_task = tokio::spawn(nostr_task(rx));
+    let cookie_path = format!("{}/.cookie", args.datadir);
+    let cookie_content = fs::read_to_string(&cookie_path)
+        .with_context(|| format!("Failed to read cookie file at {}", cookie_path))
+        .unwrap();
 
-    thread::spawn(move || zmq_task(tx));
+    let (user, password) = cookie_content
+        .trim()
+        .split_once(':')
+        .ok_or_else(|| anyhow!("Invalid cookie format"))
+        .unwrap();
 
-    if let Err(e) = tokio::try_join!(nostr_task) {
-        error!("Task exited unexpectedly: {:?}", e);
+    let bitcoin_rpc = BitcoinClient::new(
+        args.bitcoinrpc.to_string(),
+        user.to_string(),
+        password.to_string(),
+        None,
+        None,
+    )
+    .map_err(|e| anyhow!("Failed to create client: {}", e))
+    .unwrap();
+
+    let (tx, rx) = mpsc::channel(1000);
+
+    let nostr_handle = tokio::spawn(nostr_task(rx, bitcoin_rpc));
+
+    let zmq_args = args.zmq.clone();
+    thread::spawn(move || zmq_task(tx, zmq_args));
+
+    if let Err(e) = nostr_handle.await {
+        error!("Nostr task failed: {:?}", e);
     }
 }
 
-async fn nostr_task(mut rx: mpsc::Receiver<Vec<u8>>) {
+async fn nostr_task(mut rx: mpsc::Receiver<Vec<u8>>, bitcoin_rpc: BitcoinClient) {
     let client = Client::new(Keys::generate());
     client.add_relay("wss://relay.damus.io").await.unwrap();
     client.add_relay("wss://nostr.wine").await.unwrap();
     client.add_relay("wss://nos.lol").await.unwrap();
     client.connect().await;
 
+    let client_rx = client.clone();
     let bitcoin_tx_kind = Kind::Custom(28333);
 
-    let filter = Filter::new().kind(bitcoin_tx_kind).since(Timestamp::now());
-
-    let magic_value = "f9beb4d9".to_string(); // Mainnet yolo
-
-    let client_clone = client.clone();
     tokio::spawn(async move {
-        while let Some(tx_data) = rx.recv().await {
-            let tx_hex = hex::encode(&tx_data);
+        let filter = Filter::new().kind(bitcoin_tx_kind).since(Timestamp::now());
+        client_rx.subscribe(filter, None).await.unwrap();
 
-            let magic_tag = Tag::custom(TagKind::Custom("magic".into()), vec![magic_value.clone()]);
-
-            let transactions_tag =
-                Tag::custom(TagKind::Custom("transactions".into()), vec![tx_hex.clone()]);
-
-            let ephemeral_keys = Keys::generate();
-
-            let event_builder = EventBuilder::new(
-                bitcoin_tx_kind,
-                "", // Content MUST be empty per NIP-89
-            )
-            .tags(vec![magic_tag, transactions_tag]);
-
-            match event_builder.sign(&ephemeral_keys).await {
-                Ok(event) => {
-                    let event_id = event.id;
-                    match client_clone.send_event(event).await {
-                        Ok(_) => info!("Published NIP-89 event: {}", event_id),
-                        Err(e) => error!("Failed to publish event {}: {:?}", event_id, e),
-                    }
-                }
-                Err(e) => error!("Failed to sign event: {:?}", e),
-            }
-        }
-    });
-
-    let sub_id = client.subscribe(filter, None).await.unwrap().val;
-
-    client
-        .handle_notifications(|n| async {
-            if let RelayPoolNotification::Event {
-                subscription_id,
-                event,
-                ..
-            } = n
-            {
-                if subscription_id == sub_id {
+        client_rx
+            .handle_notifications(|n| async {
+                if let RelayPoolNotification::Event { event, .. } = n {
                     for tag in event.tags {
                         if tag.kind() == TagKind::Custom("transactions".into()) {
-                            for hex_tx in tag.to_vec().iter().skip(1) {
-                                if let Ok(tx_bytes) = hex::decode(hex_tx) {
+                            for encoded_tx in tag.to_vec().iter().skip(1) {
+                                if let Ok(tx_bytes) = BASE64_STANDARD.decode(encoded_tx) {
                                     if let Ok(tx) =
                                         Transaction::consensus_decode(&mut tx_bytes.as_slice())
                                     {
-                                        info!(
-                                            "Received transaction {} from Nostr",
-                                            tx.compute_txid()
-                                        );
+                                        info!(">> Received TXID: {}", tx.compute_txid());
+
+                                        match bitcoin_rpc.send_raw_transaction(&tx).await {
+                                            Ok(_) => info!("Submitted to node"),
+                                            Err(e) => {
+                                                error!("Failed to submit transaction: {:?}", e)
+                                            }
+                                        }
+                                    } else {
+                                        error!("Failed to decode transaction");
                                     }
                                 }
                             }
                         }
                     }
                 }
+                Ok(false)
+            })
+            .await
+            .unwrap();
+    });
+
+    let mut buffer: Vec<String> = Vec::new();
+    let mut current_size = 0;
+    const MAX_SIZE: usize = 10_000;
+
+    info!("Waiting for transactions to fill buffer...");
+
+    while let Some(tx_bytes) = rx.recv().await {
+        let tx_b64 = BASE64_STANDARD.encode(tx_bytes);
+
+        info!("Received transaction, size: {} bytes", tx_b64.len());
+
+        let tx_len = tx_b64.len();
+
+        buffer.push(tx_b64);
+        current_size += tx_len;
+
+        if current_size >= MAX_SIZE {
+            info!(
+                "Buffer full ({} bytes). Sending {} transactions...",
+                current_size,
+                buffer.len()
+            );
+
+            let tx_tag = Tag::custom(TagKind::Custom("transactions".into()), buffer.clone());
+            let magic_tag = Tag::custom(
+                TagKind::Custom("magic".into()),
+                vec!["f9beb4d9".to_string()],
+            );
+
+            let event = EventBuilder::new(bitcoin_tx_kind, "").tags(vec![magic_tag, tx_tag]);
+
+            if let Ok(signed_event) = event.sign(&Keys::generate()).await {
+                if let Err(e) = client.send_event(signed_event).await {
+                    error!("Publish failed: {:?}", e);
+                } else {
+                    info!("Successfully published batch!");
+                }
             }
-            Ok(false)
-        })
-        .await
-        .unwrap();
+
+            buffer.clear();
+            current_size = 0;
+        }
+    }
 }
 
-fn zmq_task(tx: mpsc::Sender<Vec<u8>>) {
-    let endpoint = "tcp://127.0.0.1:28332";
+fn zmq_task(tx: mpsc::Sender<Vec<u8>>, zmq_address: String) {
     let ctx = zmq::Context::new();
-    let socket = ctx.socket(zmq::SUB).expect("ZMQ socket failed");
-    socket.connect(endpoint).expect("ZMQ connect failed");
-    socket
-        .set_subscribe(b"rawtx")
-        .expect("ZMQ subscribe failed");
+    let socket = ctx.socket(zmq::SUB).expect("Socket failed");
+    socket.connect(&zmq_address).expect("Connect failed");
+    socket.set_subscribe(b"rawtx").expect("Subscribe failed");
 
-    info!("[ZMQ] Listening for transactions...");
     loop {
         if let Ok(Ok(topic)) = socket.recv_string(0) {
-            info!("[ZMQ] Received topic: {}", topic);
             if topic == "rawtx" {
                 if let Ok(payload) = socket.recv_bytes(0) {
-                    info!("[ZMQ] Received raw transaction of {} bytes", payload.len());
-                    let _ = socket.recv_bytes(0); // seq
+                    let _ = socket.recv_bytes(0); // skip sequence
                     let _ = tx.blocking_send(payload);
                 }
             }
